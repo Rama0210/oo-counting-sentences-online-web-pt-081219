1c6932d29234380c1c58d16020a254c6d1a30bb2
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-09-11 20:45:40 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,31 +1,20 @@
+require 'pry'
+
 class String
 
   def sentence?
-    if self.end_with?(".")
-      return true
-    else
-      return false
-    end
+    self.end_with?(".")
   end
 
   def question?
-    if self.end_with?("?")
-      return true
-    else
-      return false
-    end
+self.end_with?("?")
   end
 
   def exclamation?
-    if self.end_with?("!")
-      return true
-    else
-      return false
-    end
+self.end_with?("!")
   end
 
   def count_sentences
-    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
-    return new_array.length
+self.split(/[!.?]/).size
   end
 end
